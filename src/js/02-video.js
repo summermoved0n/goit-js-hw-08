b2e79eadaadc242e78eb08f884653f009fb1cf4b
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2023-10-22 16:54:06 -0400

    task 2 changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,24 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
 const iframe = document.getElementById('vimeo-player');
 const player = new Player(iframe);
+
+function getItemStorage() {
+  const savedTime = localStorage.getItem(STORAGE_KEY);
+  return savedTime ? parseFloat(savedTime) : 0;
+}
 
 player.on(
   'timeupdate',
   throttle(evt => {
     const currentTime = evt.seconds;
-    localStorage.setItem('videoplayer-current-time', currentTime);
-    // console.log(currentTime);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(currentTime));
   }, 1000)
 );
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+if (getItemStorage() > 0) {
+  player.setCurrentTime(getItemStorage());
+}
